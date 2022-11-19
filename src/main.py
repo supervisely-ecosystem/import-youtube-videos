df3bf35aecfcce796f7cdccac64f511c181adeca
German Vorozhko <vorozhkog@icloud.com>
2022-11-19 14:56:48 +0000

    working

--- src/main.py
+++ src/main.py
@@ -14,15 +14,23 @@ team_id = sly.env.team_id()
 workspace_id = sly.env.workspace_id()
 
 downloaded_video = None
+download_progress = None
 
 
 def my_hook(d):
     global downloaded_video
-    # if d["status"] == "downloading":
-    #     print("Downloading video!")
+    global download_progress
+
+    if d["status"] == "downloading":
+        if download_progress is None:
+            download_progress = sly.Progress(
+                f"Downloading {d['filename']}", total_cnt=d["total_bytes"], is_size=True
+            )
+        download_progress.set_current_value(d["downloaded_bytes"], report=True)
     if d["status"] == "finished":
         print("Downloaded!")
         downloaded_video = d["filename"]
+        download_progress.set_current_value(d["total_bytes"])
 
 
 def download(url, output_dir="data/"):
@@ -40,6 +48,8 @@ def download(url, output_dir="data/"):
 
 
 def main():
+    global download_progress
+
     remote_path = sly.env.file()
     local_path = os.path.join("src", sly.fs.get_file_name_with_ext(remote_path))
     api.file.download(team_id, remote_path, local_path)
@@ -67,6 +77,7 @@ def main():
         if normalized_url != "":
             try:
                 print(f"Start {normalized_url}")
+                download_progress = None
                 download(normalized_url, output_dir)
                 api.video.upload_path(
                     dataset_id=dataset.id,
