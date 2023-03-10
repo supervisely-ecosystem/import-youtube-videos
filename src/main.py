243046e9ffd7096adf8b79f67cea006979752d6b
vorozhkog <115161827+vorozhkog@users.noreply.github.com>
2023-03-10 23:46:44 +0000

    Youtube DL library -> pytube library (#1)
    * initial commit
    
    * wip
    
    * youtube-dl -> pytube
    
    * youtube-dl -> pytube

--- src/main.py
+++ src/main.py
@@ -1,13 +1,13 @@
 from __future__ import unicode_literals
 from dotenv import load_dotenv
 import os
-import youtube_dl
+import re
+from pytube import YouTube
 import supervisely as sly
 
-# load ENV variables for debug
-# has no effect in production
-load_dotenv("local.env")
-load_dotenv(os.path.expanduser("~/supervisely.env"))
+if sly.is_development():
+    load_dotenv("local.env")
+    load_dotenv(os.path.expanduser("~/supervisely.env"))
 
 api = sly.Api.from_env()
 team_id = sly.env.team_id()
@@ -16,43 +16,34 @@ workspace_id = sly.env.workspace_id()
 downloaded_video = None
 download_progress = None
 
+# def get_youtube_id(link):
 
-def my_hook(d):
-    global downloaded_video
-    global download_progress
+#     assert link.startswith("https://www.youtube.com/"), "Invalid YouTube link. Please use desktop format of url starting with 'https://www.youtube.com/...'"
+#     try:
+#         # Extract the video ID from the link using regular expressions
+#         video_id = re.search(r'(?<=v=)[^&#]+', link).group(0)
+#     except AttributeError:
+#         # If the regular expression doesn't match, raise an exception with a custom error message
+#         raise ValueError("Invalid YouTube link.")
 
-    if d["status"] == "downloading":
-        if download_progress is None:
-            download_progress = sly.Progress(
-                f"Downloading {d['filename']}", total_cnt=d["total_bytes"], is_size=True
-            )
-        download_progress.set_current_value(d["downloaded_bytes"], report=True)
-    if d["status"] == "finished":
-        print("Downloaded!")
-        downloaded_video = d["filename"]
-        download_progress.set_current_value(d["total_bytes"])
+#     print(f'Youtube ID is {video_id}')
+#     return video_id
 
 
 def download(url, output_dir="data/"):
-    ydl_opts = {
-        "format": "best",
-        "continue": True,
-        "outtmpl": output_dir + "%(uploader)s - %(title)s.%(ext)s",
-        "progress_hooks": [my_hook],
-        # "no-progress": True,
-        "quiet": True,
-    }
-
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([url])
+    global downloaded_video
+    yt = YouTube(str(url))
+    stream = yt.streams.get_highest_resolution()
+    # video_path = output_dir + f"/{get_youtube_id}."
+    downloaded_video = stream.download(output_path=output_dir)
 
 
 def main():
     global download_progress
 
     remote_path = sly.env.file()
-    local_path = os.path.join("src", sly.fs.get_file_name_with_ext(remote_path))
-    api.file.download(team_id, remote_path, local_path)
+    local_save_path = os.path.join("src", sly.fs.get_file_name_with_ext(remote_path))
+    api.file.download(team_id, remote_path, local_save_path)
 
     project = api.project.get_or_create(
         workspace_id=workspace_id,
@@ -60,12 +51,11 @@ def main():
         type=sly.ProjectType.VIDEOS,
     )
 
-    dataset = api.dataset.create(
-        project.id, "YouTube Videos", change_name_if_conflict=True
-    )
+    dataset = api.dataset.create(project.id, "YouTube Videos", change_name_if_conflict=True)
 
     data = []
-    with open(local_path, "r") as f:
+
+    with open(local_save_path, "r") as f:
         data = f.readlines()
 
     output_dir = "data/"
@@ -79,6 +69,7 @@ def main():
                 print(f"Start {normalized_url}")
                 download_progress = None
                 download(normalized_url, output_dir)
+                # ln -s /Users/germanvorozko/work-apps/supervisely/supervisely ./supervisely
                 api.video.upload_path(
                     dataset_id=dataset.id,
                     name=sly.fs.get_file_name_with_ext(downloaded_video),
@@ -91,7 +82,7 @@ def main():
                 sly.logger.warn(repr(e))
         progress.iter_done_report()
 
-    sly.fs.silent_remove(local_path)
+    sly.fs.silent_remove(local_save_path)
 
     print("Done")
     if sly.is_production():
